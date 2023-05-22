8cd8ae438b504aea613ece4582eb98fba6b95003
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-05-22 18:58:10 +0700

    int main pewarisan

--- pewarisan/pewarisan.cpp
+++ pewarisan/pewarisan.cpp
@@ -1,20 +1,47 @@
-// pewarisan.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+    public:
+    string nama;
+
+    orang(string pNama):
+        nama(pNama){
+        cout << "orang dibuat\n" << endl;
+        }
+    ~orang()
+    {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
+
+class pelajar : public orang {
+    public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) :
+        orang(pNama),
+        sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "Pelajar dihaous\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1. perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
